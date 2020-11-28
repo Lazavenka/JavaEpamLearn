51686110657a024fdd52ddc5838e9c30ed3fd71c
Lazavenka <lozovenko.andrey@gmail.com>
2020-11-29 00:07:13 +0300

    System.out.println correction

---
+++ src/by/roger/epamLearn/ModuleTwo/TwoDimArrTaskOne.java
@@ -0,0 +1,9 @@
+package by.roger.epamLearn.ModuleTwo;
+/*
+Дана матрица. Вывести на экран все нечетные столбцы, у которых первый элемент больше последнего.
+ */
+public class TwoDimArrTaskOne {
+    public static void main(String[] args) {
+
+    }
+}
